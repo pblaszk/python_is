f81c91d23ad7576d1ec94f230d01b4045afa1b5e
Piotr B <pblaszk@wp.pl>
2020-03-05 20:35:45 +0100

    2nd class

---
+++ homeworks/day_02.py
@@ -0,0 +1,98 @@
+#Lesson 2
+
+#Pobieranie danych od uzytkownika
+# imie = input("Jak masz na imie? ")
+# nazwisko = "Blaszkiewicz"
+# print(imie)
+# print(nazwisko)
+
+#zdanie = "piotrek ma kota"
+#Capitalize - duza litera na poczatku
+#print(zdanie.capitalize())
+
+#komentarz wielonijkowy
+# hej = '''Hej
+# milo
+# Cie
+# widziec'''
+#
+# print(hej)
+
+# liczba_1 = 123
+# liczba_2 = 4.5
+# #konwersja na liczbe
+# liczba_3 = int("15")
+
+#print(liczba_1//liczba_3)
+
+# tekst_1 = "Milo "
+# tekst_2 = "Cie widziec"
+# print(tekst_1 + tekst_2)
+
+#Znaki biale
+# zdanie_1 = "Hej\nPiotr"
+# zdanie_2 = "Hej\tPiotr"
+# zdanie_3 = "Hej\\tPiotr"
+# zdanie_4 = r"Hej\ntPiotr"
+# print(zdanie_1)
+# print(zdanie_2)
+# print(zdanie_3)
+# print(zdanie_4)
+
+
+# imie = input("Jak masz na imie? ")
+# nazwisko = "Blaszkiewicz"
+#
+# print(imie + '\n' + nazwisko)
+
+#sprawdzanie typu zmiennej
+# zmienna = input("Wpisz coś: ")
+# typ = type(zmienna)
+# print(typ)
+
+# liczba_1 = input("Liczba 1:")
+# liczba_2 = input("Liczba 2:")
+# n_1 = float(liczba_1).conjugate()
+# n_2 = float(liczba_2).conjugate()
+# print(n_1 + n_2)
+
+# wartosc_1 = input("Wartosc 1:")
+# wartosc_2 = input("Wartosc 2:")
+# n_1 = float(liczba_1)
+# n_2 = float(liczba_2)
+# print((n_1 + n_2).conjugate())
+#
+# sprawdzanie dlugosci
+# len(zmienna)
+
+#           01234567890
+# zdanie = "Ala ma kota"
+# print(zdanie[7])
+
+zdanie = "Ala ma kota!"
+# print(zdanie[1:8:2])
+# w domu pocwiczyc indeks z wartosciami ujemnymi
+#przedzial lewy otwarty, prawy zamkniety
+
+#print(zdanie[-1])
+
+
+# zdanie = "Piotrek"
+#
+# if (zdanie == "Piotrek"):
+#     print("Zgadza się")
+# else: ("Nie zgadza się")
+
+ostatni_znak = zdanie[-1]
+
+if ostatni_znak == 'a':
+    print('Nie krzycz na mnie')
+else:
+    print('Dziekuje')
+
+
+
+
+
+
+
