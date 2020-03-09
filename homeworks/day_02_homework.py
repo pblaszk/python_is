5a2b3f55fc6e906add28b22d04fe56983bfaa036
Piotr B <pblaszk@wp.pl>
2020-03-09 18:05:29 +0100

    2nd class homework part 1

--- homeworks/day_02_homework.py
+++ homeworks/day_02_homework.py
@@ -1,35 +1,37 @@
+import calendar
+#Piotr Blaszkiewicz
 # 1. Napisz program do przeliczania stopni Celsjusza na Fahrenheita (wyświetlając wzór i kolejne obliczenia)
-# input_cel=input("Podaj stopnie Celsjusza: ")
-# st_cel = float(input_cel)
-# st_far=(st_cel * 9/5) + 32
-# print("Fahrenheit = (Celsjusz * 9/5) + 32")
-# print("Fahrenheit = ("+st_cel.__str__()+" * 9/5) + 32")
-# print("Stopnie Farenheita przy "+st_cel.__str__()+" stopniach Celsjusza = "+st_far.__str__())
+input_cel=input("Podaj stopnie Celsjusza: ")
+st_cel = float(input_cel)
+st_far=(st_cel * 9/5) + 32
+print("Fahrenheit = (Celsjusz * 9/5) + 32")
+print("Fahrenheit = ("+st_cel.__str__()+" * 9/5) + 32")
+print("Stopnie Farenheita przy "+st_cel.__str__()+" stopniach Celsjusza = "+st_far.__str__())
 
 # 2. Napisz program do przeliczania stopni Fahrenheita na Celsjusza (wyświetlając wzór i kolejne obliczenia)
-# input_far=input("Podaj stopnie Farenheita: ")
-# st_far = float(input_far)
-# st_cel=(st_far - 32) * 5/9
-# print("Celsjusz = (Farenheit - 32) * 5/9")
-# print("Celsjusz = ("+st_far.__str__()+" - 32) * 5/9")
-# print("Stopnie Celsjusza przy "+st_far.__str__()+" stopniach Farenheita = "+st_cel.__str__())
+input_far=input("Podaj stopnie Farenheita: ")
+st_far = float(input_far)
+st_cel=(st_far - 32) * 5/9
+print("Celsjusz = (Farenheit - 32) * 5/9")
+print("Celsjusz = ("+st_far.__str__()+" - 32) * 5/9")
+print("Stopnie Celsjusza przy "+st_far.__str__()+" stopniach Farenheita = "+st_cel.__str__())
 
 # 3. Napisz program do obliczania pola powierzchni koła o zadanym promieniu (wyświetlając wzór i kolejne obliczenia)
-# input_r = input("Podaj długość promienia: ")
-# val_r = float(input_r)
-# val_pi = 3.14
-# print("Pole koła = Pi * r^2")
-# print("Pole koła = 3.14 * " + val_r.__str__())
-# val_pk = val_pi * (val_r * val_r)
-# print("Pole koła = " + val_pk.__str__())
+input_r = input("Podaj długość promienia: ")
+val_r = float(input_r)
+val_pi = 3.14
+print("Pole koła = Pi * r^2")
+print("Pole koła = 3.14 * " + val_r.__str__()+"^2")
+val_pk = val_pi * (val_r * val_r)
+print("Pole koła = " + val_pk.__str__())
 
 # 4. Napisz program, który poda pierwszą i ostatnią cyfrę podanej liczby
-# input_l = input("Podaj liczbę: ")
-# val_l = int(input_l)
-# print("Podano liczbę " + val_l.__str__())
-# val_pierwsza = int(str(val_l)[0])
-# val_ostatnia = int(str(val_l)[-1])
-# print("Pierwsza cyfra: " + val_pierwsza.__str__()+", ostatnia cyfra:"+val_ostatnia.__str__())
+input_l = input("Podaj liczbę: ")
+val_l = int(input_l)
+print("Podano liczbę " + val_l.__str__())
+val_pierwsza = int(str(val_l)[0])
+val_ostatnia = int(str(val_l)[-1])
+print("Pierwsza cyfra: " + val_pierwsza.__str__()+", ostatnia cyfra:"+val_ostatnia.__str__())
 
 # 5. Napisz program, który rysuje prostokąt o zadanych rozmiarach (wysokość i szerokość) za pomocą znaków:
 #     | (bok)
@@ -49,11 +51,43 @@ str_spa = " "*val_poz
 str_pio = ("\n|"+str_spa+"|")*val_pio
 str_gra = ("+"+str_poz+"+")
 print(str_gra + str_pio + "\n" + str_gra)
-# 6. Napisz do przeliczania liczby zapisanej w formacie binarnym na system dziesiętny. Załóż że wpisywane jest zawsze tylko 6 znaków 0/1, np 000110, 110010, 111111 etc.
+
+# 6. Napisz do przeliczania liczby zapisanej w formacie binarnym na system dziesiętny.
+#Załóż że wpisywane jest zawsze tylko 6 znaków 0/1, np 000110, 110010, 111111 etc.
+in_bin = input("Podaj wartość binarną, 6 znaków: ")
+out_dec = int(in_bin, 2)
+print(out_dec)
+
 # 7. Napisz program do rozpoznawania czy podane liczba jest parzysta czy nie.
+in_num=input("Podaj dowolną liczbę całkowitą: ")
+in_val = int(in_num)
+if (in_val % 2) == 0:
+    print("Liczba parzysta")
+else:
+    print("Liczna nieparzysta")
+
 # 8. Napisz program do sprawdzania czy liczba jest podzielna przez 3 lub 5 lub 7
+in_num=input("Podaj dowolną liczbę całkowitą: ")
+in_val = int(in_num)
+
+if (in_val % 3)  == 0 or (in_val % 5) == 0 or (in_val % 7) == 0:
+    print("Liczba podzielna przez 3 lub 5 lub 7")
+else:
+    print("Liczba niepodzielna przez 3 lub 5 lub 7")
+
 # 9. Napisz program do sprawdzania czy liczba jest podzielne przez 3 i 5 i 7
+in_num=input("Podaj dowolną liczbę całkowitą: ")
+in_val = int(in_num)
+if (in_val % 3) == 0 and (in_val % 5) == 0 and (in_val % 7) == 0:
+    print("Liczba podzielna przez 3 i 5 i 7")
+else:
+    print("Liczba niepodzielna przez 3 i 5 i 7")
+
 # 10. Napisz program do sprawdzania czy podany rok jest rokiem przestępnym
+in_year=input("Podaj rok: ")
+ch_year = int(in_year)
+print(calendar.isleap(ch_year))
+
 #
 # Podpowiedź: W zadaniach 7-10 należy użyć instrukcji warunkowych, wierzę że sobie poradzicie :)
 # Podpowiedź: Wzory potrzebne do wyliczeń w niektórych zadaniach na pewno znajdziecie w internecie, jeśli nie to zapraszam na Slacka.
