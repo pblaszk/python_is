0194c5630a185d6033c013436d3c5bb88593fdcb
Piotr B <pblaszk@wp.pl>
2020-03-10 20:29:32 +0100

    2nd class homework part 1

---
+++ exercises/day_03.py
@@ -0,0 +1,106 @@
+#Książka pt: "Kto w butach"
+# tytul = "Książka pt: \"Kto w butach\"."
+# tytul = 'Książka pt: "Kto w butach".'
+# tytul = "I'm"
+# tytul = 'I\'m'
+# print (tytul)
+
+# liczba = 1
+# liczba_2 = 2
+#
+# print(f'Wprowadziłeś liczbę: {liczba}. Dzięki')
+#
+# print('Wprowadziłeś liczbę: {} {}'.format(liczba, liczba_2))
+#
+# print('Wprowadziłeś liczbę: {} {a} {a}'.format(liczba, a=liczba_2))
+
+#odwrocone
+# zmienna = 'Piotr'
+# print(zmienna[::-1])
+
+
+# zestaw_liczb = range(0,10)
+# print(zestaw_liczb)
+#
+# lista = [1,2,3,4,5,6,7,8,9,10]
+# print(lista)
+#
+# lista2 = list('dwa')
+# print(lista2)
+#
+# lista3 = list(range(0,10))
+# print(lista3)
+
+# lista4 = ['zero', 1, 'trzeci', 'dom']
+# print(lista4)
+# print(lista4[::-1])
+
+# lista5 = ['zero', 1, 'trzeci', 'dom']
+# print(lista5)
+# lista5[1]='jeden'
+# print(lista5)
+# lista5.append('nowy element')
+# print(lista5)
+# del(lista5[1])
+# print(lista5)
+# lista5.remove('zero')
+# print(lista5)
+#
+# if 'dom123' in lista5:
+#     lista5.remove('dom123')
+# else:
+#     print("Nie istnieje")
+#
+#
+# if 'dom123' is 'dom123':
+#     print('Istnieje')
+# else:
+#     print("Nie istnieje")
+
+#rage - niemutowany
+#list - mutowany
+#tuple - krotka, jak list, ale niemutowany - nie mozna kasowac, dodawac
+
+# krotka = ("jeden", "dwa", "trzy")
+# # print(krotka)
+# # print(type(krotka))
+# # print(len(krotka))
+# # print(krotka[0])
+# # del(krotka)
+
+# czy_kontynuowac = 'T'
+# while czy_kontynuowac == 'T':
+#     #nieskonczona petla
+#     print('.')
+#     czy_kontynuowac = input('Czy kontynuowac? [T/N]?')
+
+licznik = 0
+# while licznik < 10:
+#     licznik += 1
+#     #licznik = licznik + 1
+#     if licznik == 5:
+#         continue
+#     else:
+#         print(licznik)
+
+# while licznik < 10:
+#     licznik += 1
+#     #licznik = licznik + 1
+#     if licznik == 5:
+#         break
+#     else:
+#         print(licznik)
+
+# zdanie = "Ala ma kota"
+# for litera in zdanie:
+#     print(litera)
+
+lista = [10, 26, 27, 28, 29]
+for element in lista:
+    print(element)
+
+
+
+
+
+
